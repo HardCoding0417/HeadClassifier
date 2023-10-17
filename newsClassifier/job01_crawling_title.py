c977e506b57f380a781307ac0d0be8204abaa98b
Younghoda <dudgh597@naver.com>
2023-10-17 15:58:53 +0900

    d22

--- newsClassifier/job01_crawling_title.py
+++ newsClassifier/job01_crawling_title.py
@@ -7,9 +7,9 @@ import pandas as pd
 import re
 import time
 import datetime
+from bs4 import BeautifulSoup
 
-category = ['General', 'News', 'Reviews', 'Tips', 'MOD']
-category_list = [0, 330, 20, 340, 350]
+
 
 url = 'https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head=0&page=1'
 
@@ -29,31 +29,47 @@ titles=[]
 
 df_titles = pd.DataFrame()
 
-for j in category_list:
-    url = 'https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head={}&page=1'.format(j)
-    print(url)
-    for k in range(1, pages[0]+1):
-        G_url = 'https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head={}&page={}'.format(j,k)
-        driver.get(G_url)
-        time.sleep(0.5)  # 페이지를 바꿀 시간을 줘야한다 없으면 에러가 발생할 수 있음.
-        for i in range(2, 47):
-            try:
-                title = driver.find_element('xpath',
-                                            '//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr[{}]/td[3]/a[1]'.format(i)).text
-                # title = re.compile('').sub('', title))
-                #          # ('[^가-힣]]').sub('', title))
-                titles.append(title)
-            except:
-                print("error {}".format(i))
-            df_section_title = pd.DataFrame(titles, columns=['titles'])
-            df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
-            df_titles.to_csv('../crawling_data/head_.csv', index=False)
-            titles = []
+def func(t):
+    category = ['Normal', 'News', 'Review', 'Tip', 'Mod']
+    category_list = [0, 330, 20, 340, 350]
+    for j in category_list[t]:
+        url = 'https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head={}&page=1'.format(j)
+        # print(url)
+        for k in range(1, pages[0]+1):
+            G_url = 'https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head={}&page={}'.format(j,k)
+            driver.get(G_url)
+            time.sleep(0.5)  # 페이지를 바꿀 시간을 줘야한다 없으면 에러가 발생할 수 있음.
+            for i in range(2, 47):
+                try:
+                    title = driver.find_element('xpath',
+                                                '//*[@id="container"]/section[1]/article[2]/div[2]/table/tbody/tr[{}]/td[3]/a[1]'.format(i)).test
+                    # title = re.compile('').sub('', title))
 
-# https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head=0&page=1                 일반
-# https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head=330&page=1   소식
-# https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head=20&page=1    후기
-# https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head=340&page=1   팁
-# https://gall.dcinside.com/mgallery/board/lists/?id=mouse&sort_type=N&search_head=350&page=1   모드
+                    titles.append(title)
+                except:
+                    print("error {}".format(i))
+                df_section_title = pd.DataFrame(titles, columns=['titles'])
+                df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
+                df_titles.to_csv('../crawling_data/head_.csv', index=False)
+                titles = []
+
+    for i in range(1, 2):
+        driver.get('https://gall.dcinside.com/mgallery/board/lists/?id=mouse&page={}'.format(i))
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        links = soup.select('a[href^="/mgallery/board/view/"]')
+        for link in links:
+            print(f'글 링크: {link}')
+        time.sleep(0.5)
+
+    return 0
+
+
+list = [0,1,2]
+func(list[0])
+
+
+
+
 
 
