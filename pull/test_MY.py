d9a16223f34f398ddd32e9c7f169b6ec28b20daf
Terive0302 <terive@naver.com>
2023-10-19 11:58:06 +0900

    model folder in two pickle file crawling_data floder in two csv file test_MY and MY2 changes code

--- pull/test_MY.py
+++ pull/test_MY.py
@@ -2,7 +2,8 @@ import pandas as pd
 import glob
 import datetime
 
-data_path = glob.glob('../MoonJeangHwan/*.csv')
+# data_path = glob.glob('../MoonJeangHwan/*.csv')
+data_path = glob.glob('../crawling_data/DC_v2.csv')
 print(data_path)
 
 df = pd.DataFrame()
@@ -10,7 +11,7 @@ for path in data_path:
     df_temp = pd.read_csv(path)
     df = pd.concat([df, df_temp])
 print(df.head())
-print(df['말머리'].value_counts())
+# print(df['titles'].value_counts())
 df.info()
 df.to_csv('../crawling_data/dcinside_{}.csv'.format(
     datetime.datetime.now().strftime('%Y%m%d')), index=False, encoding='UTF-8-sig')
--- pull/test_MY2.py
+++ pull/test_MY2.py
@@ -10,17 +10,18 @@ import pickle
 
 pd.set_option('display.unicode.east_asian_width', True)
 # 한국어 맞춤 글자 폭 으로 세팅
-df = pd.read_csv('../crawling_data/dcinside_20231018.csv')
+df = pd.read_csv('../crawling_data/dcinside_20231019.csv')
 # test_MY에서 만든 csv파일을 읽음
 # print(df)
 # print("------------------------------")
 # df.info()
 # print("------------------------------")
 
-X = df['본문']    # okt객체로 만들어서 형태소 분리해야 할 내용
-Y = df['말머리']   # 카테고리
+X = df['text']    # okt객체로 만들어서 형태소 분리해야 할 내용
+Y = df['category']   # 카테고리
 
 print(X) # 본문 내용이 출력됨
+print("----------------------------------------")
 print(Y) # 카테고리가 출력됨
 
 encoder = LabelEncoder() # 범주형 데이터를 정수형으로 변경해주는 클래스
@@ -40,9 +41,16 @@ print(onehot_y)
 
 okt = Okt() # Open korea text 한국어 형태소 분석 클래스 객체 만들기
 
+# test
+# print(okt.morphs(X[102], stem=True), "테스트를 위한 형태소")
+
 for i in range(len(X)): # X는 본문 데이터프레임
     # okt_morph_x = okt.morphs(X[0], stem = True)
-    X[i] = okt.morphs(X[i], stem=True)
+    try:
+        X[i] = okt.morphs(X[i], stem=True)
+    except:
+        print("error {}".format(X[i]))
+
     # X[i]에 들어온 텍스트를 형태로 단위로 분석
     # stem=True 옵션을 통해 어간 형태로 변경
     # stem = True를 주면 원형으로 바꿔줌 (하다)
@@ -53,21 +61,27 @@ print(X[i], "\nOKT 프린팅")
 stopwords = pd.read_csv('../stopwords.csv', index_col = 0)
 for j in range(len(X)): # X는 본문 데이터프레임, 본문의 갯수만큼 반복
     words = []
-    for i in range(len(X[j])): # 본문 데이터프레임의 인덱싱, 그 안에 들어있는 텍스트 길이만큼 반복
-        if len(X[j][i]) > 1: # 1글자 이상 조건문
-            if X[j][i] not in list(stopwords['stopword']): # stopwords csv파일에 없는 단어
-                words.append(X[j][i]) # 그 단어들만 추가
-    X[j] = ' '.join(words)
+    try:
+        for i in range(len(X[j])):  # 본문 데이터프레임의 인덱싱, 그 안에 들어있는 텍스트 길이만큼 반복
+            if len(X[j][i]) > 1:  # 1글자 이상 조건문
+                if X[j][i] not in list(stopwords['stopword']):  # stopwords csv파일에 없는 단어
+                    words.append(X[j][i])  # 그 단어들만 추가
+        X[j] = ' '.join(words)
+    except:
+        print("error {}".format(j))
 for i in range(3):
     print(X[i])
     print('\n')
+
+    # 오류 잡음
+X = [str(x) for x in X]
 
 token = Tokenizer()
 token.fit_on_texts(X)
 tokened_x = token.texts_to_sequences(X)
 wordsize = len(token.word_index) + 1
 print(tokened_x[:])
-print(wordsize, "워드사이즈입니다.")
+print(wordsize, "워드사이즈입니다.") # 13270
 
 with open('../models/dc_token.pickle', 'wb') as f:
     pickle.dump(token, f) # 토큰을 f에다 저장
@@ -76,15 +90,16 @@ max = 0
 for i in range(len(tokened_x)):
     if max < len(tokened_x[i]):
         max = len(tokened_x[i])
-print(max, "맥스사이즈입니다.")
+print(max, "맥스사이즈입니다.") # 990
 
 x_pad = pad_sequences(tokened_x, max) # 문장을 주고, 길이를 주면 모자란 갯수만큼 0을 채워줌
 print(x_pad[:])
 
-# X_train, X_test, Y_train, Y_test = train_test_split(
-#     x_pad, onehot_y, test_size=0.2)
-# print(X_train.shape, Y_train.shape)
-# print(X_test.shape, Y_test.shape)
-#
-# xy = X_train, X_test, Y_train, Y_test
-# np.save('./crawling_data/news_data_max_{}_wordsize_{}'.format(max, wordsize), xy)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    x_pad, onehot_y, test_size=0.2)
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
+
+xy = X_train, X_test, Y_train, Y_test
+np.save('../crawling_data/dcinside_data_max_{}_wordsize_{}'.format(max, wordsize), xy,
+        allow_pickle=True)
