20e68cf19a067dc4e8dcdd186725e02cec5e54c5
Terive0302 <terive@naver.com>
2023-10-19 12:04:33 +0900

    path change

--- pull/test_MY.py
+++ pull/test_MY.py
@@ -3,7 +3,7 @@ import glob
 import datetime
 
 # data_path = glob.glob('../MoonJeangHwan/*.csv')
-data_path = glob.glob('../crawling_data/DC_v2.csv')
+# data_path = glob.glob('../crawling_data/DC_v2.csv')
 print(data_path)
 
 df = pd.DataFrame()
